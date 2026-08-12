56959c33b199214f2defb8742807a0da0273a7b8
Emmanueladimchinaobi <deborahamybliss@gmail.com>
2026-08-12 23:14:33 +0100

    first commit

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ const closeBtn = document.getElementById("closeBtn");
 const mobileMenu = document.getElementById("mobileMenu");
 
 menuBtn.addEventListener("click", () => {
-  mobileMenu.classList.remove("hidden");
-  mobileMenu.classList.add("flex");
+ mobileMenu.classList.remove("hidden");
+ mobileMenu.classList.add("flex");
+ 
+ 
 });
 
 closeBtn.addEventListener("click", () => {
-  mobileMenu.classList.remove("flex");
   mobileMenu.classList.add("hidden");
+  mobileMenu.classList.remove("remove");
+  
+  
+  
 });
 
 const servicesBtn = document.getElementById("services-btn");
@@ -41,3 +46,71 @@ const servicesmenu2 = document.getElementById ("services-menu2");
 servicesbtn2.addEventListener ("click", () => {
     servicesmenu2.classList.toggle("hidden");
 });
+
+
+//*******products */
+
+const products = document.querySelectorAll(".product");
+const pagination = document.getElementById("pagination");
+
+
+const productsPerPage = 4;
+
+let currentPage = 1;
+
+const totalPages = Math.ceil(products.length / productsPerPage);
+
+
+function showProducts(page) {
+
+    currentPage = page;
+
+    const start = (page - 1) * productsPerPage;
+    const end = start + productsPerPage;
+
+    products.forEach((product, index) => {
+
+        if (index >= start && index < end) {
+
+            product.classList.remove("hidden");
+
+        } else {
+
+            product.classList.add("hidden");
+
+        }
+
+    });
+
+    createPagination();
+
+}
+
+
+function createPagination() {
+
+    pagination.innerHTML = "";
+
+    for (let i = 1; i <= totalPages; i++) {
+
+        const button = document.createElement("button");
+
+        button.textContent = i;
+
+        button.className =
+            "px-4 py-2 mx-1 rounded bg-gray-200 hover:bg-pink-600 hover:text-white  active:bg-red-700 transition-all duration-300";
+
+        button.addEventListener("click", () => {
+
+            showProducts(i);
+
+        });
+
+        pagination.appendChild(button);
+
+    }
+
+}
+
+
+showProducts(1);
